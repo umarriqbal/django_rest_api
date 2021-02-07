f7eff6c40ceca3e86f07f50d58a8171c93415c53
Umar Iqbal <umari@theentertainerasia.com>
2021-02-07 17:22:40 +0500

    init admin stuff

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -10,8 +10,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
 
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
 
+    is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     is_merchant = models.BooleanField(default=False)
 
